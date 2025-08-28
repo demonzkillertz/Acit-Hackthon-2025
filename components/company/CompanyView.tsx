ae038b74a6d047f790e5ed517ecfe9e79cfd8174
Sanjib.Kasti.39 <kastisanjib@gmail.com>
2025-08-28 20:05:20 +0545

    view file added

---
+++ components/company/CompanyView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CompanyView = () => (
+  <View>
+    <Text>Company View (Dummy)</Text>
+  </View>
+);
+
+export default CompanyView;
---
+++ components/user/UserView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UserView = () => (
+  <View>
+    <Text>User View (Dummy)</Text>
+  </View>
+);
+
+export default UserView;
