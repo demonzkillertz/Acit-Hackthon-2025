bafd3868e0e37abd33f0de8237ce9846b7b5be96
Siddhartha Manandhar <abantinostha@gmail.com>
2025-08-29 02:53:37 +0545

    3done

--- app/company/CompanyTabs.tsx
+++ app/company/CompanyTabs.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CompanyView from '../../components/company/CompanyView';
+import Company2 from '../../components/company/Company2';
+import About from '../../components/company/About';
 import { View, Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
@@ -17,7 +19,9 @@ export default function CompanyTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="CompanyHome" component={CompanyView} options={{ title: 'Company Home' }} />
-      <Tab.Screen name="CompanyTab2" component={DummyCompanyTab} options={{ title: 'Tab 2' }} />
+      <Tab.Screen name="CompanyTab2" component={Company2} options={{ title: 'Company2' }} />
+      <Tab.Screen name="CompanyTab3" component={DummyCompanyTab} options={{ title: 'Tab 3' }} />
+      <Tab.Screen name="CompanyTab4" component={About} options={{ title: 'About' }} />
     </Tab.Navigator>
   );
 }
